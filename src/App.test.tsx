fd7a26c43dfda2658afa67b9d8dd729c3cb8aaf7
farshad <far3farazdel@gmail.com>
2023-12-19 18:08:52 +0330

    first deployment

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,12 @@
+
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+
+// test that learn react exist in documnetation or not
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
 });
